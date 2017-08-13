560abe457bc0f4a445e4b1a787ba054d7e5e5b44
Fransy <fransy@systemsforall.com>
2017-08-13 16:17:37 +0100

    Add ACF example in About page

--- src/scripts/components/About.js
+++ src/scripts/components/About.js
@@ -3,9 +3,11 @@ import DataStore from 'flux/stores/DataStore.js'
 class About extends React.Component {
     render() {
         let page = DataStore.getPageBySlug('about');
+        let acf = page.acf; // Advanced Custom Fields data
         return (
             <div>
                 <h1>{page.title.rendered}</h1>
+                <h2>{acf.page_subtitle}</h2>
             </div>
         );
     }
